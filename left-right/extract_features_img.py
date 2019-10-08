cc28a8fa848e4abcf5486342027b6258baaf72c8
Aidan Peruch <aper9336@gmail.com>
2019-10-08 19:24:32 +1100

    Autoencoder and CNN models
    Added an unfinished autoencoder implementation, a working CNN realtime model, some test data

---
+++ left-right/extract_features_img.py
@@ -0,0 +1,48 @@
+'''
+Created on 2017.12.9
+
+@author: Richard
+'''
+
+import os
+import librosa.display
+import librosa.feature
+import numpy as np
+import matplotlib.pyplot as plt
+
+feature = 'nothing'
+
+def extract_mfcc(in_path, file, fmax, nMel):
+    y, sr = librosa.load(in_path + file)
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=nMel, fmax=fmax)
+    
+    plt.figure(figsize=(3, 3), dpi=100)
+    librosa.display.specshow(librosa.amplitude_to_db(S), fmax=fmax)
+
+    plt.xticks([])
+    plt.yticks([])
+    plt.tight_layout()
+    plt.savefig('orig_features/' + feature + '/' + file[:-3] + 'png', bbox_inches='tight', pad_inches=-0.1)
+    
+    plt.close()   
+    return
+
+
+count = 0       # number of files processed
+
+in_path = 'orig_data/' + feature + '/'       # input directory# in_path = '222/'
+
+count = 0
+for wavfile in os.listdir(in_path):
+    
+    # Input file
+    S = extract_mfcc(in_path, wavfile, 8000, 256)
+    # print(wavfile)
+    
+    # Count processed files
+    count += 1
+    if count % 20 == 0:
+        print ('%d files processed.' % count)
+
+print ('Done!\t%d files processed.' % count)
+
---
+++ left-right/generate_tr_silence.py
@@ -0,0 +1,91 @@
+import numpy as np
+import pyaudio
+import struct
+import wave
+
+import os
+
+BLOCKSIZE = 128
+
+# Get recording parameters
+RATE     = 22050
+WIDTH    = 2
+CHANNELS = 1
+LEN      = 1 * RATE
+
+def is_silent(data, THRESHOLD):
+    "Returns 'True' if below the threshold"
+    return max(data) < THRESHOLD
+
+def record(path):
+    
+    # Output wave file
+    output_wf = wave.open('orig_data/' + path, 'w')
+    # output_wf = wave.open('222/-1_richard_22.wav', 'w')
+    output_wf.setframerate(RATE)
+    output_wf.setsampwidth(WIDTH)
+    output_wf.setnchannels(CHANNELS)
+
+    p = pyaudio.PyAudio()
+    stream = p.open(format = p.get_format_from_width(WIDTH),
+                    channels = CHANNELS,
+                    rate = RATE,
+                    input = True,
+                    output = True)
+    
+    # Wait until voice detected
+    while True:    
+        input_string = stream.read(BLOCKSIZE, exception_on_overflow = False)
+        input_value = struct.unpack('h' * BLOCKSIZE, input_string)
+        silent = is_silent(input_value, 0)
+        if not silent:
+            break
+
+        # Start recording
+    print("Recording Start")
+
+    nBLOCK = int(LEN / BLOCKSIZE)
+    numSilence = 0
+    for n in range(0, nBLOCK + 1):
+        
+        if is_silent(input_value, 100):
+            numSilence += 1
+#            output_value = np.zeros(BLOCKSIZE) 
+ 
+        output_value = np.array(input_value)
+        
+        if numSilence > 14:
+            break
+        
+        output_value = output_value.astype(int)
+        output_value = np.clip(output_value, -2**15, 2**15 - 1)
+
+        ouput_string = struct.pack('h' * BLOCKSIZE, *output_value)
+        output_wf.writeframes(ouput_string)
+        
+        input_string = stream.read(BLOCKSIZE, exception_on_overflow = False)
+        input_value = struct.unpack('h' * BLOCKSIZE, input_string)
+
+    print('Recording End')
+
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    output_wf.close()
+
+name = "nothing-2"
+
+print("Record your \"nothing\" samples")
+
+import time
+ts = time.time()
+import datetime
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+
+for x in range(500):
+    ts = time.time()
+    st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d-%H-%M-%S')
+    print("Ready", x,"/500")
+    left_filename = "nothing/{}{}.wav".format(name, st)
+    record(left_filename)
+    os.system('cls')  # For Windows
